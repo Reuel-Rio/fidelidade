801f2b17db8509bcdb4030467489a06b77a9693f
Reuel B. Albuquerque Jr <reuelja@gmail.com>
2023-04-23 17:27:50 -0300

    primeiro

---
+++ Fidelidade/src/main/java/com/multiluz/fidelidade/FidelidadeApplication.java
@@ -0,0 +1,13 @@
+package com.multiluz.fidelidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FidelidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FidelidadeApplication.class, args);
+	}
+
+}
---
+++ Fidelidade/src/test/java/com/multiluz/fidelidade/FidelidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multiluz.fidelidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FidelidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
